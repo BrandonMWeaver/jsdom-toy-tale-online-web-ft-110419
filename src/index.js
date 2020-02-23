40421e2c6a5b021fcc94f4f7ddee2150589da1af
Brandon Weaver <a7xncob@gmail.com>
2020-02-23 21:17:46 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -34,7 +34,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }).then(function(response) {
       return response.json();
     }).then(function(object) {
+      let h2 = document.createElement('h2')
+      h2.innerText = toy.name
       
+      let img = document.createElement('img')
+      img.setAttribute('src', toy.image)
+      img.setAttribute('class', 'toy-avatar')
+      
+      let p = document.createElement('p')
+      p.innerText = `${toy.likes} likes`
+      
+      let btn = document.createElement('button')
+      btn.setAttribute('class', 'like-btn')
+      btn.setAttribute('id', toy.id)
+      btn.innerText = "like"
+      btn.addEventListener('click', function(event) {
+        console.log(event.target.dataset);
+        likes(event);
+      });
+      
+      let divCard = document.createElement("div");
+      divCard.setAttribute("class", "card");
+      divCard.append(h2, img, p, btn);
+      divCollect.append(divCard);
     });
   }
 });
